64ee3c4d2ffaa6ac7ff10f51189a5584b4de3ceb
kcy230426 <132032089+kcy230426@users.noreply.github.com>
2024-07-26 16:03:10 +0900

    add
    add

---
+++ books1.js
@@ -0,0 +1,4 @@
+[{id:3, price:5000, title:"체인소맨 4권"},
+    {id:4, price:5500, title:"체인소맨 5권"},
+    {id:5, price:5500, title:"체인소맨 6권"}
+]
---
+++ books2.js
@@ -0,0 +1,4 @@
+[{id:6, price:5500, title:"체인소맨 7권"},
+    {id:7, price:5500, title:"체인소맨 8권"},
+    {id:8, price:5500, title:"체인소맨 9권"}
+]
